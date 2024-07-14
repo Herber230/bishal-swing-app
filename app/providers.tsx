9e8070a868d03d7dad4cf1f6b1f2d13241669e58
Herber Colop <herber16ext@gmail.com>
2024-07-14 01:10:56 -0600

    feat: initialize styles with nextui (#7)

---
+++ app/providers.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import * as React from 'react';
+import { NextUIProvider } from '@nextui-org/system';
+
+export interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+export function Providers({ children }: ProvidersProps) {
+  return <NextUIProvider>{children}</NextUIProvider>;
+}
