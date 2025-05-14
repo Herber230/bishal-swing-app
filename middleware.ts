f9f1b130b0b657eb6f2164177cebda86240a6ff9
Herber230 <herber16ext@gmail.com>
2025-05-13 22:34:50 -0600

    feat(core): themed Header

--- middleware.ts
+++ middleware.ts
@@ -43,5 +43,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|images|favicon.ico).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|images|icons|favicon.ico).*)'],
 };
--- src/implementation/components/molecules/language-selector/language-selector.tsx
+++ src/implementation/components/molecules/language-selector/language-selector.tsx
@@ -1,28 +1,89 @@
 'use client';
 
-import type { JSX } from 'react';
+import { useState } from 'react';
 import { usePathname, useRouter } from 'next/navigation';
-import { Button, ButtonGroup } from '@heroui/button';
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+} from '@heroui/react';
+import Image from 'next/image';
 
-export function LanguageSelector(): JSX.Element {
+const languages = [
+  {
+    label: 'EN',
+    value: 'en',
+    flagSource: '/icons/usa-flag.svg',
+    flagAlt: 'English Flag',
+  },
+  {
+    label: 'ES',
+    value: 'es',
+    flagSource: '/icons/spain-flag.svg',
+    flagAlt: 'Spanish Flag',
+  },
+] as const;
+
+type LangValue = (typeof languages)[number]['value'];
+
+function LanguageItem({ lang }: { lang: (typeof languages)[number] }) {
+  return (
+    <div className="flex">
+      <Image
+        src={lang.flagSource}
+        alt={lang.flagAlt}
+        width={20}
+        height={20}
+        className="mr-2"
+      />
+      {lang.label}
+    </div>
+  );
+}
+
+const sanitizeLang = (currentPath: string) => {
+  const langRegex = new RegExp(`^/(${languages.map(l => l.value).join('|')})`);
+  const match = currentPath.match(langRegex);
+
+  if (!match) {
+    throw new Error('Invalid language in the path');
+  }
+  return match[1] as (typeof languages)[number]['value'];
+};
+
+export function LanguageSelector(): React.JSX.Element {
   const pathname = usePathname();
   const router = useRouter();
+  const [selectedLang, setSelectedLang] = useState(sanitizeLang(pathname));
 
-  const handleClick = (lang: string) => {
-    const updatedPath = pathname.replace(/\/(en|es)/, `/${lang}`);
+  const handleClick = (lang: LangValue) => {
+    const updatedPath = pathname.replace(`/${selectedLang}`, `/${lang}`);
+    setSelectedLang(lang);
     router.push(updatedPath);
   };
 
   return (
-    <div>
-      <ButtonGroup>
-        <Button variant="light" onPress={() => handleClick('en')}>
-          English
+    <Dropdown className="data-[slot=content]:min-w-0 [&_li]:p-0">
+      <DropdownTrigger>
+        <Button variant="light">
+          <LanguageItem lang={languages.find(l => l.value === selectedLang)!} />
         </Button>
-        <Button variant="light" onPress={() => handleClick('es')}>
-          Español
-        </Button>
-      </ButtonGroup>
-    </div>
+      </DropdownTrigger>
+      <DropdownMenu items={languages}>
+        {lang => (
+          <DropdownItem key={`lang-key-${lang.value}`}>
+            <Button
+              variant="light"
+              onPress={() => handleClick(lang.value)}
+              className="flex w-full justify-start"
+            >
+              <LanguageItem lang={lang} />
+            </Button>
+          </DropdownItem>
+        )}
+      </DropdownMenu>
+    </Dropdown>
   );
 }
