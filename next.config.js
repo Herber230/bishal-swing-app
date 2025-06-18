6702f4908ae60ed6613c1dcae39b9adf261877c8
Herber Colop <herber16ext@gmail.com>
2025-06-17 21:45:53 -0600

    chore: init configurarion for deployment on main branch (#27)

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin('./server/i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone',
+};
 
 module.exports = withNextIntl(nextConfig);
